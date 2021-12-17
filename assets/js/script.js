c95360a3c39ff0bb538d4a2ab48c53195d115b43
dani <photomassacre@gmail.com>
2021-12-16 16:55:34 -0800

    Updating the js to add a task to the to-do

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 var taskItemEl = document.createElement("li");
+var formEl = document.querySelector("#task-form");
 
-var buttonEl = document.querySelector("#save-task");
+// var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = () => {
+var createTaskHandler = (event) => {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+// buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
